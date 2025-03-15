8e1f4214b9a61d918f9b01d16d22d968fba15be9
The-dog-dude <857624@dpsk12.net>
2025-03-15 15:40:07 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Wait for the page to fully load
-window.addEventListener('load', () => {
-    // Hide the loading screen once the page has finished loading
-    document.getElementById('loading-screen').style.display = 'none';
-
-    // Show the main content
-    document.getElementById('content').style.display = 'block';
-});
